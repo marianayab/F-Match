07b770aa3cbc1da97910d3131d464e58687cb093
marianayab <128246098+marianayab@users.noreply.github.com>
2024-01-16 01:26:28 +0500

    added index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+const cards = document.querySelectorAll(".card");
+var isFlipped = false;
+var card1;
+var card2;
+
+
+cards.forEach((card) => card.addEventListener("click", flip));
+
+
+function flip() {
+
+  this.classList.add("flip");
+  if (!isFlipped) {
+
+    isFlipped = true;
+    card1 = this;
+
+  }
+
+  else if (card1 === this) {
+    return;
+  }
+
+  else {
+
+    card2 = this;
+    check();
+
+  }
+}
+
+
+
+function check() {
+
+  if (card1.dataset.image === card2.dataset.image) {
+    success();
+  }
+
+  else {
+    fail();
+  }
+}
+
+
+function success() {
+  console.log("matched");
+  card1.removeEventListener("click", flip);
+  card2.removeEventListener("click", flip);
+  reset();
+
+}
+
+function fail() {
+  setTimeout(() => {
+    card1.classList.remove("flip");
+    card2.classList.remove("flip");
+    reset();
+  }, 300);
+
+}
+
+function reset() {
+  isFlipped = false;
+  card1 = null;
+  card2 = null;
+}
+
+
+(function shuffle() {
+
+  cards.forEach((card) => {
+    var index = Math.floor(Math.random() * 16);
+    card.style.order = index;
+  });
+
+})();
+
+
+
